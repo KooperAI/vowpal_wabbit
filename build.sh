ca856df245759bd0092f708c171455fdb65efbf9
Francisco J. Solis <siscomagma@gmail.com>
2021-02-24 22:28:20 -0600

    Updated build.sh and CMakeLists (WIP)
    Now a subset of the build is had, but commandline.o error at the build is a point to get working.

--- build.sh
+++ build.sh
@@ -1,1 +1,9 @@
- emcmake cmake .. -DBOOST_ROOT=`pwd`/../../boost_1_71_0 -DBoost_INCLUDE_DIR=`pwd`/../../boost_1_71_0 -DBoost_LIBRARY_DIR=`pwd`/../../boost_1_71_0/stage/lib  -DZLIB_INCLUDE_DIR=`pwd`/../../zlib -DZLIB_LIBRARY=`pwd`/../../zlib/build -DZLIB_INCLUDE_DIR="`pwd`/../../zlib/build;`pwd`/../../zlib" -DBUILD_FLATBUFFERS=OFF -DBUILD_TESTS=OFF -DSTATIC_LINK_VW=ON
+emcmake cmake .. \
+  -DBOOST_ROOT=`pwd`/../../boost_1_71_0 \
+  -DBoost_INCLUDE_DIR=`pwd`/../../boost_1_71_0 \
+  -DBoost_LIBRARY_DIR=`pwd`/../../boost_1_71_0/stage/lib \
+  -DZLIB_LIBRARY=`pwd`/../../zlib/build/libz.a \
+  -DZLIB_INCLUDE_DIR="`pwd`/../../zlib/build;`pwd`/../../zlib" \
+  -DBUILD_FLATBUFFERS=OFF \
+  -DBUILD_TESTS=OFF \
+  -DSTATIC_LINK_VW=ON
