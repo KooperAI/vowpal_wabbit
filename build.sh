60373273e296d84394e4056fb311fbb8b359096b
Francisco J. Solis <siscomagma@gmail.com>
2021-03-01 15:50:25 -0600

    Modified building script for using mutlithreading
    The build.sh script was modified in order to reflect the commands needed for building the library and examples using emscripten with multithreading enabled.

--- build.sh
+++ build.sh
@@ -1,15 +1,21 @@
 # Boost must be cloned and compiled with
 #  ./bootstraph.sh
-#  ./b2 toolset=emscripten link=static variant=release threading=single runtime-link=static program_options system
+#  ./b2 toolset=emscripten link=static variant=release threading=multi \
+#  runtime-link=static cxxflags="-s USE_PTHREADS=1" \
+#  linkflags="--shared-memory -Wl -pthread" \
+#  program_options system
+#  
+# After that, each output directory should run emar q libboost_NAME.a *.bc
 # ZLIB must be compiled with emcmake and emmake as well
 
 emcmake cmake .. \
   -DBOOST_ROOT=`pwd`/../../boost_1_71_0 \
   -DBoost_INCLUDE_DIR=`pwd`/../../boost_1_71_0 \
-  -DBoost_LIBRARY_DIR="`pwd`/../../boost_1_71_0/bin.v2/libs/system/build/emscripten-2.0.14/release/link-static/visibility-hidden/;`pwd`/../../boost_1_71_0/bin.v2/libs/program_options/build/emscripten-2.0.14/release/link-static/visibility-hidden/" \
+  -DBoost_LIBRARY_DIR="`pwd`/../../boost_1_71_0/bin.v2/libs/system/build/emscripten-2.0.14/release/link-static/threading-multi/visibility-hidden/;`pwd`/../../boost_1_71_0/bin.v2/libs/program_options/build/emscripten-2.0.14/release/link-static/threading-multi/visibility-hidden/" \
   -DZLIB_LIBRARY=`pwd`/../../zlib/build/libz.a \
   -DZLIB_INCLUDE_DIR="`pwd`/../../zlib/build;`pwd`/../../zlib" \
   -DBUILD_FLATBUFFERS=OFF \
   -DBUILD_TESTS=OFF \
   -DSTATIC_LINK_VW=ON \
-  -DCMAKE_EXE_LINKER_FLAGS="-s LLD_REPORT_UNDEFINED"
+  -DCMAKE_CXX_FLAGS="-s USE_PTHREADS=1" \
+  -DCMAKE_EXE_LINKER_FLAGS="-s LLD_REPORT_UNDEFINED -pthread"
